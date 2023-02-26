e24cb4987789638aeb0671f12cc7615eec3c72a6
Nixgit2000 <fourwaycoordination@gmail.com>
2023-02-26 15:26:55 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
